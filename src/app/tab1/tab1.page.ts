9ccdc5146b0df5c57c25cd3345c602b4c9ce2ca6
oka hidenobu <okahidenobu@gmail.com>
2019-10-22 16:07:15 +0900

    保存処理

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {MyProfile,FathersProfile,MothersProfile} from '../interfaces/my-profile';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
+
+  myProfile: MyProfile;
+  FathersProfile:FathersProfile;
+  MothersProfile:MothersProfile;
 
   constructor() {}
+
+
+testClick(){
+  console.log('click')
+}
+
+ngOnInit(){
+  // データを取得
+  this.myProfile.nickname = 'me';
+  this.myProfile.birth = 'mybirthdate';
+  this.myProfile.gender = 'boy';
+  this.myProfile.area = 'fukuoka';
+
+  this.FathersProfile.nickname = 'myfather';
+  this.FathersProfile.birth = 'fatersbirthdate';
+  this.FathersProfile.emailAdress = 'father@family.com';
+
+  this.MothersProfile.nickname = 'myfather';
+  this.MothersProfile.birth = 'mothersbirthdate';
+  this.MothersProfile.emailAdress = 'mother@family.com';
+
+
+    //localstrgaeに保存
+  localStorage.setItem('my_profile', JSON.stringify('my_profile'));
+  localStorage.setItem('MyFathersProfile', JSON.stringify('MyFathersProfile'));
+  localStorage.setItem('MyMothersProfile', JSON.stringify('MyMothersProfile'));
+}
 
 }
